f13f15c9da985028818e97263463537e47ff38ee
Sergio Pimentel <sergio.pimentel.perez@gmail.com>
2026-05-24 15:34:03 -0600

    feat(sergio): add LLVM function and symbol codegen

--- analisis.py
+++ analisis.py
@@ -587,6 +587,196 @@ def p_error(p):
     raise SyntaxError(f"Syntax error near '{p.value}' at line {p.lineno}")
 
 
+class IRGenerator(Visitor):
+    def __init__(self) -> None:
+        self.module = ir.Module(name="prog")
+        self.stack = []
+        self.functions = {}
+        self.globals = {}
+        self.symbol_tables = []
+        self.function_return_types = {}
+        self.current_function = None
+        self.builder = None
+        self.break_stack = []
+        self.continue_stack = []
+        self.string_count = 0
+        self.printf = self.declare_printf()
+
+    def declare_printf(self):
+        int_type = ir.IntType(32)
+        char_ptr = ir.IntType(8).as_pointer()
+        fnty = ir.FunctionType(int_type, [char_ptr], var_arg=True)
+        return ir.Function(self.module, fnty, name="printf")
+
+    def llvm_type(self, type_name: str):
+        if type_name in ("int", "boolean", "bool"):
+            return ir.IntType(32)
+        if type_name == "float":
+            return ir.DoubleType()
+        if type_name == "char":
+            return ir.IntType(8)
+        if type_name == "void":
+            return ir.VoidType()
+        raise TypeError(f"Unknown type '{type_name}'")
+
+    def push_scope(self) -> None:
+        self.symbol_tables.append({})
+
+    def pop_scope(self) -> None:
+        self.symbol_tables.pop()
+
+    def define_symbol(self, name: str, ptr, type_name: str) -> None:
+        self.symbol_tables[-1][name] = (ptr, type_name)
+
+    def find_symbol(self, name: str):
+        for table in reversed(self.symbol_tables):
+            if name in table:
+                return table[name]
+        if name in self.globals:
+            return self.globals[name]
+        raise NameError(f"Variable '{name}' was not declared")
+
+    def cast(self, value, target_type):
+        if value.type == target_type:
+            return value
+        if isinstance(target_type, ir.DoubleType) and isinstance(value.type, ir.IntType):
+            return self.builder.sitofp(value, target_type)
+        if isinstance(value.type, ir.DoubleType) and isinstance(target_type, ir.IntType):
+            return self.builder.fptosi(value, target_type)
+        if isinstance(value.type, ir.IntType) and isinstance(target_type, ir.IntType):
+            if value.type.width < target_type.width:
+                return self.builder.zext(value, target_type)
+            return self.builder.trunc(value, target_type)
+        return value
+
+    def to_bool(self, value):
+        if isinstance(value.type, ir.DoubleType):
+            return self.builder.fcmp_ordered("!=", value, ir.Constant(value.type, 0.0))
+        if isinstance(value.type, ir.IntType):
+            return self.builder.icmp_signed("!=", value, ir.Constant(value.type, 0))
+        return value
+
+    def promote(self, lhs, rhs):
+        if isinstance(lhs.type, ir.DoubleType) or isinstance(rhs.type, ir.DoubleType):
+            double = ir.DoubleType()
+            return self.cast(lhs, double), self.cast(rhs, double)
+        if isinstance(lhs.type, ir.IntType) and isinstance(rhs.type, ir.IntType):
+            width = max(lhs.type.width, rhs.type.width, 32)
+            target = ir.IntType(width)
+            return self.cast(lhs, target), self.cast(rhs, target)
+        return lhs, rhs
+
+    def alloca_at_entry(self, name: str, type_name: str):
+        return self.builder.alloca(self.llvm_type(type_name), name=name)
+
+    def current_block_has_terminator(self) -> bool:
+        return self.builder.block.terminator is not None
+
+    def visit_program(self, node: Program) -> None:
+        for item in node.declarations:
+            if isinstance(item, list):
+                for declaration in item:
+                    self.declare_global(declaration)
+            elif isinstance(item, (Function, FunctionPrototype)):
+                self.declare_function(item)
+        for item in node.declarations:
+            if isinstance(item, Function):
+                item.accept(self)
+
+    def declare_global(self, node: Declaration) -> None:
+        llvm_type = self.llvm_type(node.type)
+        glob = ir.GlobalVariable(self.module, llvm_type, name=node.variable)
+        glob.initializer = ir.Constant(llvm_type, 0)
+        self.globals[node.variable] = (glob, node.type)
+
+    def declare_function(self, node: Function) -> None:
+        if node.name in self.functions:
+            return
+        ret_type = self.llvm_type(node.return_type)
+        arg_types = [self.llvm_type(param.type) for param in node.params]
+        fnty = ir.FunctionType(ret_type, arg_types)
+        func = ir.Function(self.module, fnty, name=node.name)
+        for llvm_arg, param in zip(func.args, node.params):
+            llvm_arg.name = param.name
+        self.functions[node.name] = func
+        self.function_return_types[node.name] = node.return_type
+
+    def visit_function(self, node: Function) -> None:
+        func = self.functions[node.name]
+        self.current_function = func
+        entry = func.append_basic_block("entry")
+        self.builder = ir.IRBuilder(entry)
+        self.push_scope()
+
+        for arg, param in zip(func.args, node.params):
+            ptr = self.alloca_at_entry(param.name, param.type)
+            self.builder.store(arg, ptr)
+            self.define_symbol(param.name, ptr, param.type)
+
+        for declaration in node.declarations:
+            declaration.accept(self)
+        for statement in node.statements:
+            statement.accept(self)
+            if self.current_block_has_terminator():
+                break
+
+        if not self.current_block_has_terminator():
+            if node.return_type == "void":
+                self.builder.ret_void()
+            else:
+                self.builder.ret(ir.Constant(self.llvm_type(node.return_type), 0))
+
+        self.pop_scope()
+        self.current_function = None
+        self.builder = None
+
+    def visit_declaration(self, node: Declaration) -> None:
+        if node.size is not None:
+            size = self.const_int(node.size)
+            ptr = self.builder.alloca(ir.ArrayType(self.llvm_type(node.type), size), name=node.variable)
+        else:
+            ptr = self.alloca_at_entry(node.variable, node.type)
+        self.define_symbol(node.variable, ptr, node.type)
+        if node.initializer is not None and node.size is None:
+            value = node.initializer.accept(self)
+            self.builder.store(self.cast(value, self.llvm_type(node.type)), ptr)
+
+    def const_int(self, node) -> int:
+        if isinstance(node, Literal) and node.type == "int":
+            return int(node.value)
+        raise TypeError("Array sizes must be integer literals")
+
+    def visit_block(self, node: Block) -> None:
+        self.push_scope()
+        for declaration in node.declarations:
+            declaration.accept(self)
+        for statement in node.statements:
+            statement.accept(self)
+            if self.current_block_has_terminator():
+                break
+        self.pop_scope()
+
+    def visit_emptystatement(self, node: EmptyStatement) -> None:
+        return None
+
+    def location_ptr(self, node):
+        if isinstance(node, Variable):
+            return self.find_symbol(node.name)
+        if isinstance(node, ArrayRef):
+            ptr, type_name = self.find_symbol(node.name)
+            zero = ir.Constant(ir.IntType(32), 0)
+            index = self.cast(node.index.accept(self), ir.IntType(32))
+            elem_ptr = self.builder.gep(ptr, [zero, index], inbounds=True)
+            return elem_ptr, type_name
+        raise TypeError("Invalid assignment target")
+
+    def visit_assignment(self, node: Assignment):
+        ptr, type_name = self.location_ptr(node.target)
+        value = node.expression.accept(self)
+        value = self.cast(value, self.llvm_type(type_name))
+        self.builder.store(value, ptr)
+        return value
+
 
 
 def build_parser():
@@ -601,11 +791,30 @@ def parse(source: str) -> Program:
 
 def compile_source(source: str) -> str:
     root = parse(source)
-    return str(root)
+    irgen = IRGenerator()
+    root.accept(irgen)
+    return str(irgen.module)
+
+
+data = """
+int factorial(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int result;
+    result = factorial(5);
+    printf("factorial(5) = %d\\n", result);
+    return result;
+}
+"""
 
 
 if __name__ == "__main__":
-    source = ""
+    source = data
     if len(sys.argv) > 1:
         with open(sys.argv[1], encoding="utf8") as file:
             source = file.read()
